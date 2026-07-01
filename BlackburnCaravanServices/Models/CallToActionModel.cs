2f4312f78288be96c9f57a6db1c29c1f56680af0
Simon Roberts <simongtt@googlemail.com>
2026-07-01 14:19:12 +0100

    rework css and CTA

---
+++ BlackburnCaravanServices/Models/CallToActionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlackburnCaravanServices.Models;
+
+public sealed record CallToActionModel(
+    string Eyebrow,
+    string Title,
+    string Description,
+    string ButtonText,
+    string Url
+);
---
+++ BlackburnCaravanServices/Pages/Shared/_CallToAction.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlackburnCaravanServices.Pages.Shared;
+
+public class _CallToAction : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
