e1b34f2fc566fa5de0b8819ef23a20b9abbb48d2
Simon Roberts <simongtt@googlemail.com>
2026-07-01 13:45:01 +0100

    Tidy site css

---
+++ BlackburnCaravanServices/Models/PageHeroModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlackburnCaravanServices.Models;
+
+public sealed record PageHeroModel(
+    string Eyebrow,
+    string Title,
+    string Description,
+    IReadOnlyList<PageHeroFeatureModel> Features
+);
+
+public sealed record PageHeroFeatureModel(
+    string Title,
+    string Text
+);
---
+++ BlackburnCaravanServices/Pages/_PageHero.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlackburnCaravanServices.Pages;
+
+public class _PageHeroModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
